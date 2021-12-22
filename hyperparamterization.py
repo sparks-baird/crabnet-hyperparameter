05b82258abcf070f51c2772696f16e5b254cf82b
mliu7051 <58011963+mliu7051@users.noreply.github.com>
2021-12-21 17:01:52 -0700

    Update hyperparamterization.py

--- hyperparamterization.py
+++ hyperparamterization.py
@@ -15,27 +15,14 @@ from ax.service.managed_loop import optimize
 from ax.utils.notebook.plotting import render, init_notebook_plotting
 from ax.utils.tutorials.cnn_utils import load_mnist, train, evaluate, CNN
 from crabnet.train_crabnet import get_model
+from crabnet.data.materials_data import elasticity
+from crabnet.model import data
+from sklearn.metrics import mean_squared_error
 
 init_notebook_plotting()
 
-torch.manual_seed(12345)
-dtype = torch.float
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-BATCH_SIZE = 512
-train_loader, valid_loader, test_loader = load_mnist(batch_size=BATCH_SIZE)
-
-from crabnet.model import data
-train_df, val_df = data(elasticity, "train.csv")
+train_df, val_df = data(elasticity)
 train_df = train_df[:100]
-
-from crabnet.train_crabnet import get_model
-
-train_true, train_pred, formulas, train_sigma = crabnet_model.predict(train_df)
-
-from sklearn.metrics import mean_squared_error
-
-mse = mean_squared_error(train_true, train_pred)
 
 def mse_error(parameterization):
     crabnet_model = get_model(
@@ -43,17 +30,17 @@ def mse_error(parameterization):
     train_df=train_df,
     learningcurve=False,
     force_cpu=False,
-    parameters = parameterization
+    emb_scaler = parameterization.get("emb_scaler")
     )
     train_true, train_pred, formulas, train_sigma = crabnet_model.predict(train_df)
     mse = mean_squared_error(train_true, train_pred)
-    return mse
+    return {"error": mse}
 
 best_parameters, values, experiment, model = optimize(
     parameters=[
-        {"name": "lr", "type": "range", "bounds": [1e-6, 0.4], "log_scale": True},
-        {"name": "momentum", "type": "range", "bounds": [0.0, 1.0]},
+        {"name": "emb_scaler", "type": "range", "bounds": [0, 10]}
     ],
+    experiment_name="hyperparameterization",
     evaluation_function=mse_error,
     objective_name='error',
     minimize = True
