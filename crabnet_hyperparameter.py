46a87fe6ad0c22237a6fbbc776ee6666c023ff02
sgbaird <sterling.baird@icloud.com>
2021-12-28 22:10:18 -0700

    Update crabnet_hyperparameter.py

--- crabnet_hyperparameter.py
+++ crabnet_hyperparameter.py
@@ -10,15 +10,14 @@
 # In[1]:
 
 
+import torch
 import numpy as np
 
 from ax.plot.contour import plot_contour
 from ax.plot.trace import optimization_trace_single_method
 from ax.service.managed_loop import optimize
-from ax.metrics.branin import branin
-from ax.utils.measurement.synthetic_functions import hartmann6
 from ax.utils.notebook.plotting import render, init_notebook_plotting
-
+from ax.utils.tutorials.cnn_utils import load_mnist, train, evaluate, CNN
 from crabnet.train_crabnet import get_model
 from crabnet.data.materials_data import elasticity
 from crabnet.model import data
@@ -33,15 +32,29 @@ init_notebook_plotting()
 # In[2]:
 
 train_df, val_df = data(elasticity)
-mdl = get_model(emb_scaler=1.0)
+train_df = train_df[:100]
 
-parameters = ["emb_scaler"]
+crabnet_model = get_model(emb_scaler=1.0)
+
+train_true, train_pred, formulas, train_sigma = crabnet_model.predict(train_df)
 
 
 def crabnet_evaluation_function(parameterization):
-    x = np.array([parameterization.get(parameter) for parameter in parameters])
-    # In our case, standard error is 0, since we are computing a synthetic function.
-    return {"mae": hartmann6(x)}
+    crabnet_model = get_model(
+    mat_prop="elasticity",
+    train_df=train_df,
+    learningcurve=False,
+    force_cpu=False,
+    parameters = parameterization
+    )
+    train_true, train_pred, formulas, train_sigma = crabnet_model.predict(train_df)
+    mse = mean_squared_error(train_true, train_pred)
+    return mse
+
+# def crabnet_evaluation_function(parameterization):
+#     x = np.array([parameterization.get(parameter) for parameter in parameters])
+#     # In our case, standard error is 0, since we are computing a synthetic function.
+#     return {"mae": hartmann6(x)}
 
 
 # If there is only one metric in the experiment – the objective – then evaluation function can return a single tuple of mean and SEM, in which case Ax will assume that evaluation corresponds to the objective. It can also return only the mean as a float, in which case Ax will treat SEM as unknown and use a model that can infer it. For more details on evaluation function, refer to the "Trial Evaluation" section in the docs.
