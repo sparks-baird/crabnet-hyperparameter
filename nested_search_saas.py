0cdb0793f96aa3070c2d2f65d57c052d2e69ed19
sgbaird <sterling.baird@icloud.com>
2022-02-12 18:40:37 -0700

    Update nested_search_saas.py

--- nested_search_saas.py
+++ nested_search_saas.py
@@ -1,26 +1,17 @@
 # %% imports
 # NOTE: `pip install pyro-ppl` to use FULLYBAYESIAN (SAASBO)
+from time import time
+from os.path import join
 from pathlib import Path
+import numpy as np
 
 import pandas as pd
 
 from matbench.bench import MatbenchBenchmark
 
-import plotly.graph_objects as go
-import plotly.express as px
-from ax.plot.marginal_effects import plot_marginal_effects
-from ax.plot.feature_importances import (
-    plot_feature_importance_by_feature_plotly,
-    plot_feature_importance_by_metric_plotly,
-)
-from ax.plot.parallel_coordinates import (
-    prepare_experiment_for_plotting,
-    plot_parallel_coordinates_plotly,
-)
-from ax.plot.slice import plot_slice_plotly, interact_slice_plotly
-
 import torch
 
+from ax.storage.json_store.save import save_experiment
 from ax import RangeParameter, ChoiceParameter, ParameterType, Data
 from ax.core import (
     SearchSpace,
@@ -29,6 +20,7 @@ from ax.core import (
     OptimizationConfig,
     Objective,
 )
+from ax.storage.metric_registry import register_metric
 from ax.core.parameter_constraint import SumConstraint, OrderConstraint
 from ax.runners.synthetic import SyntheticRunner
 from ax.modelbridge.registry import Models
@@ -39,13 +31,22 @@ from utils.matbench import get_test_results
 from utils.parameterization import crabnet_mae
 
 # %% setup
-dummy = True
+dummy = False
 metric = "crabnet_mae"
 
 if dummy:
     n_splits = 2
+    n_sobol = 2
+    n_saas = 3
+    num_samples = 16
+    warmup_steps = 32
 else:
     n_splits = 5
+    # n_sobol = 2 * len(search_space.parameters)
+    n_sobol = 10
+    n_saas = max(100 - n_sobol, 0)
+    num_samples = 256
+    warmup_steps = 512
 
 torch.manual_seed(12345)  # To always get the same Sobol points
 tkwargs = {
@@ -54,8 +55,12 @@ tkwargs = {
 }
 
 # create dir https://stackoverflow.com/a/273227/13697228
-experiment_dir = "experiments"
-figure_dir = "figures"
+parameter_str = join("saas", f"sobol_{n_sobol}-saas_{n_saas}")
+experiment_dir = join("experiments", parameter_str)
+figure_dir = join("figures", parameter_str)
+if dummy:
+    experiment_dir = join(experiment_dir, "dummy")
+    figure_dir = join(figure_dir, "dummy")
 Path(experiment_dir).mkdir(parents=True, exist_ok=True)
 Path(figure_dir).mkdir(parents=True, exist_ok=True)
 
@@ -190,25 +195,22 @@ class CrabNetMetric(Metric):
         return Data(df=pd.DataFrame.from_records(records))
 
 
-# %% matbench loop
-if dummy:
-    n_sobol = 2
-    n_saas = 3
-else:
-    n_sobol = 2 * len(search_space.parameters)
-    n_saas = max(100 - n_sobol, 0)
+register_metric(metric_cls=CrabNetMetric)
 
+# %% matbench loop
 mb = MatbenchBenchmark(autoload=False, subset=["matbench_expt_gap"])
 
 task = list(mb.tasks)[0]
 task.load()
+maes = []
 for i, fold in enumerate(task.folds):
+    t0 = time()
     train_inputs, train_outputs = task.get_train_and_val_data(fold)
     train_val_df = pd.DataFrame(
         {"formula": train_inputs.values, "target": train_outputs.values}
     )
     if dummy:
-        train_val_df = train_val_df[:100]
+        train_val_df = train_val_df[:25]
 
     optimization_config = OptimizationConfig(
         objective=Objective(
@@ -217,83 +219,68 @@ for i, fold in enumerate(task.folds):
     )
     # TODO: use status_quo (Arm) as default CrabNet parameters
     exp = Experiment(
-        name="nested_crabnet_mae",
+        name="nested_crabnet_mae_saas",
         search_space=search_space,
         optimization_config=optimization_config,
         runner=SyntheticRunner(),
     )
 
     sobol = Models.SOBOL(exp.search_space)
+    print("evaluating SOBOL points")
     for _ in range(n_sobol):
+        print(_)
         trial = exp.new_trial(generator_run=sobol.gen(1))
         trial.run()
         trial.mark_completed()
 
     best_arm1 = None
-    for _ in range(n_saas):
+    data = exp.fetch_data()
+    j = -1
+    new_value = np.nan
+    best_so_far = np.nan
+    for j in range(n_saas):
         saas = Models.FULLYBAYESIAN(
             experiment=exp,
             data=exp.fetch_data(),
-            num_samples=256,  # Increasing this may result in better model fits
-            warmup_steps=512,  # Increasing this may result in better model fits
+            num_samples=num_samples,  # Increasing this may result in better model fits
+            warmup_steps=warmup_steps,  # Increasing this may result in better model fits
             gp_kernel="rbf",  # "rbf" is the default in the paper, but we also support "matern"
             torch_device=tkwargs["device"],
             torch_dtype=tkwargs["dtype"],
             verbose=False,  # Set to True to print stats from MCMC
-            disable_progbar=False,  # Set to False to print a progress bar from MCMC
+            disable_progbar=True,  # Set to False to print a progress bar from MCMC
         )
         generator_run = saas.gen(1)
         best_arm, _ = generator_run.best_arm_predictions
         trial = exp.new_trial(generator_run=generator_run)
         trial.run()
         trial.mark_completed()
+        data = Data.from_multiple_data([data, trial.fetch_data()])
+        new_value = trial.fetch_data().df["mean"].min()
+        best_so_far = data.df["mean"].min()
+        tf = time()
+        print(
+            f"fold{i}, BestInIter:{new_value:.3f}, BestSoFar:{best_so_far:.3f} elapsed time: {tf - t0}",
+        )
 
     exp.fetch_data()
     best_parameters = best_arm.parameters
 
-    fig = plot_feature_importance_by_feature_plotly(saas)
-    fig.show()
-
-    fig = plot_feature_importance_by_metric_plotly(saas)
-    fig.show()
-
-    fig = plot_marginal_effects(saas, metric)
-    data = fig[0]["data"]
-    layout = fig[0]["layout"]
-    fig = go.Figure({"data": data, "layout": layout})
-    fig.show()
-
-    exp_df = prepare_experiment_for_plotting(exp)
-    out_df = exp.fetch_data().df
-    exp_df[metric] = out_df["mean"].values
-
-    # need to install stats_model for trendlines, see
-    # https://www.statsmodels.org/stable/install.html
-    # 1D parameter projections
-    fig = px.scatter(
-        exp_df,
-        x="batch_size",
-        y=metric,
-        trendline="lowess",
-        trendline_options=dict(frac=0.25),
-        trendline_scope="overall",
-    )
-    fig.show()
-
-    fig = plot_parallel_coordinates_plotly(exp)
-    fig.show()
-
-    fig = interact_slice_plotly(saas)
-    fig.show()
+    experiment_fpath = join(experiment_dir, "experiment" + str(i) + ".json")
+    save_experiment(exp, experiment_fpath)
 
     test_pred, default_mae, test_mae, best_parameterization = get_test_results(
         task, fold, best_parameters, train_val_df
     )
+    maes.append(test_mae)
 
     task.record(fold, test_pred, params=best_parameterization)
+
+print(maes)
+print(np.mean(maes))
 my_metadata = {"algorithm_version": crabnet.__version__}
 mb.add_metadata(my_metadata)
-mb.to_file("expt_gap_benchmark.json.gz")
+mb.to_file(join(figure_dir, "expt_gap_benchmark.json.gz"))
 1 + 1
 
 # %% Code Graveyard
