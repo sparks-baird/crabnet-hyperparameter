bc96e3a67e68172bc785ba3bae7f50fbfa2029fe
sgbaird <sterling.baird@icloud.com>
2022-02-15 19:38:59 -0700

    Update saas_submitit.py

--- saas_submitit.py
+++ saas_submitit.py
@@ -11,7 +11,8 @@ print(f"dummy: {dummy}")
 # %% submission
 log_folder = "log_ax/%j"
 walltime = 10  # 4320 min == 3 days
-partition, account = ["notchpeak-gpu-guest", "notchpeak-gpu-guest"]
+partition, account = ["notchpeak-gpu-guest", "owner-gpu-guest"]
+# partition, account = ["notchpeak-gpu", "notchpeak-gpu"]
 # partition, account = ["notchpeak-guest", "owner-guest"]
 executor = AutoExecutor(folder=log_folder)
 executor.update_parameters(
