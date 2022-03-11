5fa0b70a77c4d6279ca3001668bb09342c4d7abe
sgbaird <sterling.baird@icloud.com>
2022-03-11 10:53:56 -0700

    add paper plotting and move Ax experiments

---
+++ paper_plotting.py
@@ -0,0 +1,12 @@
+"""Reproduce paper figures for crabnet-hyperparameter."""
+# %% load data
+# default
+from os.path import join
+
+exp_dir = "experiments"
+fig_dir = "figures"
+default_dir = join(exp_dir, "default", )
+
+# Ax
+
+# SAASBO
